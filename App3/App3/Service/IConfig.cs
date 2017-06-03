32c186bed9f536bb2ddd01d95cc902ddd6451fa1
Guilherme Gorski <guilhermevgorski@gmail.com>
2017-06-03 15:41:56 -0300

    atualização banco de dados

---
+++ App3/App3/Service/IConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite.Net.Interop;
+
+namespace App3
+{
+   public interface IConfig
+    {
+        string DiretorioDB { get; }
+
+        ISQLitePlatform Plataforma { get; }
+    }
+}
